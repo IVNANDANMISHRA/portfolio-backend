1daffaa84d335ff998b0a2084e651a0f5a60e4fe
S Nandan Mishra <115075926+IVNANDANMISHRA@users.noreply.github.com>
2023-11-06 00:48:08 +0530

    Update

--- index.js
+++ index.js
@@ -1,48 +1,60 @@
 const express = require('express');
-const Redis = require('ioredis');
+const { Pool } = require('pg');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const bodyParser = require('body-parser'); // Add this line
+
+dotenv.config();
+
 const app = express();
-const port = process.env.PORT || 3000;
+app.use(cors())
+const port = 8000;
 
-// Replace with your KV_URL
-const redis = new Redis('redis://default:96608c69411c45b3858558184547daa9@on-goose-45166.upstash.io:45166');
-
-// Middleware for parsing JSON requests
-app.use(express.json());
-
-// Store data
-app.post('/store', async (req, res) => {
-  try {
-    const { key, value } = req.body;
-    await redis.set(key, value);
-    res.status(201).json({ message: 'Data stored successfully' });
-  } catch (error) {
-    res.status(500).json({ error: 'Error storing data' });
-  }
+const pool = new Pool({
+    user: process.env.POSTGRES_USER,
+    host: process.env.POSTGRES_HOST,
+    database: process.env.POSTGRES_DATABASE,
+    password: process.env.POSTGRES_PASSWORD,
+    port: 5432,
+    ssl: { rejectUnauthorized: false }
 });
 
-// Retrieve data
-app.get('/retrieve/:key', async (req, res) => {
-  try {
-    const key = req.params.key;
-    const value = await redis.get(key);
-    if (value !== null) {
-      res.json({ key, value });
-    } else {
-      res.status(404).json({ error: 'Key not found' });
-    }
-  } catch (error) {
-    res.status(500).json({ error: 'Error retrieving data' });
-  }
+app.use(bodyParser.json()); // Use body-parser to parse JSON requests
+
+app.get('/getContacts', (req, res) => {
+    pool.query('SELECT * FROM contacts', (error, results) => {
+        if (error) {
+            console.error('Error executing query', error);
+            res.status(500).send('Internal Server Error');
+        } else {
+            res.json(results.rows);
+        }
+    });
 });
 
-app.get("/",(req,res)=>{
+app.post('/addContact', (req, res) => {
+    const { name, email,message } = req.body;
+
+    // Insert the data into the 'contacts' table
+    const query = 'INSERT INTO contacts (name, email,message) VALUES ($1, $2,$3)';
+    const values = [name, email,message];
+
+    pool.query(query,values, (error) => {
+        if (error) {
+            console.error('Error executing query', error);
+            res.status(500).send('Internal Server Error');
+        } else {
+            res.status(201).send('Contact added successfully');
+        }
+    });
+});
+
+app.get("/",async(req,res)=>{
 try {
   res.status(200).json({ error: 'Hi Portfolio is up' });
 } catch (error) {
   res.status(500).json({ error: 'I am down' });
-}
-})
-
+}});
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
